b94b24c1801e175572d658b7e5ae2fbb517f3e2a
Joren Miner <joren.ideas@gmail.com>
2019-07-27 08:56:27 -0600

    update dashboard

--- pivdi-dashboard/src/Main.js
+++ pivdi-dashboard/src/Main.js
@@ -12,17 +12,24 @@ class Main extends Component {
     render() {
         return (
             <HashRouter>
-                <h1>piVDI</h1>
-                <ul className="header">
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/vms">VMs</NavLink></li>
-                    <li><NavLink to="/new">Create VM</NavLink></li>
-                </ul>
-                <div className="content">
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/vms" component={VMs}/>
-                    <Route path="/new" component={New}/>
+                <div className="grid-container">
+                    <header className="header">
+                        <h1 className="header__title">piVDI Virtual Desktops</h1>
+                    </header>
+                    <aside className="sidenav">
+                        <ul className="sidenav__list">
+                            <NavLink to="/" className="sidenav__list-text"><li className="sidenav__list-item">Home</li></NavLink>
+                            <NavLink to="/vms" className="sidenav__list-text"><li className="sidenav__list-item">VMs</li></NavLink>
+                              
+                        </ul>
+                    </aside>
+                    <div className="content">
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/vms" component={VMs}/>
+                        <Route path="/new" component={New}/>
+                    </div>
                 </div>
+                
             </HashRouter>
         )
     }
